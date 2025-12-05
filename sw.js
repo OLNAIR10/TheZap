7b90848a287e13e5daac6458c779e934f158319d
Olnair Pereira <155587472+OLNAIR10@users.noreply.github.com>
2025-12-05 05:24:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,12 @@
-self.addEventListener("push", function (event) {
+self.addEventListener("push", (event) => {
   let data = {};
+
   try {
     data = event.data.json();
-  } catch (e) {
-    data = { title: "Nova mensagem", body: "Você recebeu algo!" };
-  }
+  } catch {}
 
-  const options = {
-    body: data.body,
-    icon: data.icon || "/icon.png",
-    badge: "/icon.png",
-    vibrate: [200, 100, 200],
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title || "Notificação", options)
-  );
-});
-
-self.addEventListener("notificationclick", function (event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow("/") // abre seu app ao tocar na notificação
-  );
+  self.registration.showNotification(data.title || "Notificação", {
+    body: data.body || "Você recebeu uma mensagem",
+    icon: "/icon.png"
+  });
 });
